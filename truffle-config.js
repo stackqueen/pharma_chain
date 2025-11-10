2ed087700ac6dd3534916bc977eef7cabd3b6b8f
Sleshapanda <slesha1panda48@gmail.com>
2025-11-10 12:36:06 +0530

    Add files via upload

---
+++ truffle-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: 5777,
+      gas: 6721975,
+      gasPrice: 20000000000
+    }
+  },
+
+  compilers: {
+    solc: {
+      version: "0.8.19",
+      settings: {
+        optimizer: { enabled: true, runs: 200 }
+      }
+    }
+  }
+};
